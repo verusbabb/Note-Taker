81729d7184c9fdf70b98910e47a396adcba030b7
Steve Babb <steve.babb@outlook.com>
2021-02-25 10:07:05 -0600

    Added unique ID package/cleaned up comments

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uniqid = require('uniqid');
 
 // Sets up the Express App
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./Develop/public'));
 
+//Grabs data from the data file
 var storedData = fs.readFileSync('./Develop/db/db.json');
 var storedNotes = JSON.parse(storedData);
 console.log(storedNotes);
@@ -19,40 +21,24 @@ console.log(storedNotes);
 // Basic route that sends the user first to the Get Started Page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
 
-//Route that sends user to the Notes page
+//Route route that responds with notes.html page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
 
 
-// Route that displays all notes
+// Route that responds with notes as requested
 app.get('/api/notes', (req, res) => { res.json(storedNotes) })
 
 
-// Route that creates new notes; assigns them a unique ID to enable app.delete to delete the selected object in the storedNotes array
+// Route that write new notes to data file after assigning a unique ID to the note and returns the note for html appending.
 app.post('/api/notes', (req, res) => {
   let newNote = req.body
-
-
-  // This allows the test note to be the original note.
-  let highestId = 99;
-  // This loops through the array and finds the highest ID.
-  for (let i = 0; i < storedNotes.length; i++) {
-    let individualNote = storedNotes[i];
-
-    if (individualNote.id > highestId) {
-      // highestId will always be the highest numbered id in the notesArray.
-      highestId = individualNote.id;
-    }
-  }
-  // This assigns an ID to the newNote. 
-  newNote.id = highestId + 1;
-
+  newNote.id = uniqid();
 
   storedNotes.push(newNote);
   let storedData = JSON.stringify(storedNotes);
   console.log(storedNotes);
 
   fs.writeFile('./Develop/db/db.json', storedData, finished);
-
 
   function finished(err) {
     console.log('New note added!');
@@ -62,22 +48,13 @@ app.post('/api/notes', (req, res) => {
   res.json(newNote)
 });
 
-//FROM CARLY
+//Route that deletes notes from data file as requested
 app.delete('/api/notes/:id', (req, res) => {
   console.log(req.params.id)
-  let note = storedNotes.find(({ id }) => id === JSON.parse(req.params.id));
+  let note = storedNotes.find(({ id }) => id === (req.params.id));
   storedNotes.splice(storedNotes.indexOf(note), 1);
   res.end("Note deleted")
 })
-//END FROM CARLY
-
-//ATTEMPTING REWRITE TRYING TO USE TEST RATHER THAN ID (DOESN'T WORK)
-// app.delete('/api/notes/:title', (req, res) => {
-//   console.log(req.params.title)
-//   let note = storedNotes.find(({ title }) => title === req.params.title);
-//   storedNotes.splice(storedNotes.indexOf(note), 1);
-//   res.end("Note deleted")
-// })
 
 // Starts the server to begin listening
 
