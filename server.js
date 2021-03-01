1c7d3de2fd5c5c301403e3cb9b51dce1c25c503c
Steve Babb <steve.babb@outlook.com>
2021-03-01 08:12:39 -0600

    add final comments

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
+//Assigning required dependencies
 const express = require('express');
-
 const htmlRoutes = require('./Routes/htmlRoutes')
 const apiRoutes = require('./Routes/apiRoutes');
 
 // Sets up the Express App
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,6 +14,6 @@ app.use(express.static('./Develop/public'));
 
 app.use(apiRoutes);
 app.use(htmlRoutes);
-// Starts the server to begin listening
 
+// Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
